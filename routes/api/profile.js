75bf5ffbe7736ea476dad741437f9665a83165a5
VIKRAM KUMAR <vkbiotech841@gmail.com>
2020-08-31 01:12:39 +0530

    Get github repos api added

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const request = require('request');
+const config = require('config');
 const auth = require("../../middleware/auth");
 const { body, validationResult } = require('express-validator');
 
 const Profile = require('../../models/Profile');
 const User = require("../../models/User");
+const { response } = require("express");
 
 
 // @route   GET api/profile/me
@@ -326,6 +329,33 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
     }
 });
 
+// @route   GET api/profile/github/:username
+// @desc    Get user repos from Github
+// @acess   Public
+
+router.get('/github/:username', (req, res) => {
+    try {
+        const options = {
+            uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&
+            sort=created:asc&client_id=${config.get('githubClientId')}&
+            client_secret=${config.get('githubSecret')}`,
+            method: 'GET',
+            headers: { 'user-agent': 'node.js' }
+        }
+        request(options, (error, response, body) => {
+            if (error) console.error(error);
+
+            if (response.statusCode !== 200) {
+                return res.status(400).json({ msg: 'No Github profile found' });
+            }
+
+            res.json(JSON.parse(body));
+        })
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 
 
 module.exports = router;
